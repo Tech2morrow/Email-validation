eb074d9edd60195bd0f691cc640233da9f47c525
MUHAMMED NABEEL M <nabeelin1994@gmail.com>
2018-12-18 17:46:32 +0530

    Update email12.py

--- email12.py
+++ email12.py
@@ -5,6 +5,6 @@ a=input("Enter email ID : ")
 match = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', a)
 
 if match == None:
-    print('Bad Syntax')
+    print('Email validation failure ..')
 else:
-    print('Good')
+    print('Email is in required format.')
